5f56537f0e69a4fbdc606188a8d04f6001cd1185
SoyaR08 <rafa140200@gmail.com>
2025-10-15 12:41:13 +0200

    Finished, exercices 11 to 20

--- array-exercises/11to20exercices.py
+++ array-exercises/11to20exercices.py
@@ -28,3 +28,44 @@ print("Cadena concatenada:", single_string)
 
 numbers = [1, 2, 3, 4, 5, 6]
 print("Longitud de la lista de números:", len(numbers))
+
+# Ejercicio 14
+
+chars = ['a', 'b', 'c', 'd', 'e']
+print("Índice de e:", chars.index('e'))
+
+# Ejercicio 15
+
+list1 = [1, 2, 3]
+list2 = ["a", "b", "c"]
+list1.extend(list2)
+print("Lista después de usar extend():", list1)
+
+# Ejercicio 16
+
+print("Últimos 3 elementos de la lista:", chars[-3:])
+
+# Ejercicio 17
+
+colors = ["rojo", "verde", "azul"]
+colors_copy = colors.copy()
+print("Copia de la lista de colores:", colors_copy)
+
+# Ejercicio 18
+
+father_list = [colors, numbers, chars]
+for list in father_list:
+    print("Elemento de una lista interna:", list[0])
+    break
+
+# Ejercicio 19
+
+print("Array 3 antes de la limpieza:", array3)
+array3.clear()
+print("Array 3 después de la limpieza:", array3)
+
+# Ejercicio 20
+
+elements = [1, 2, 3]
+multiplied_list = lambda x: [i * 4 for i in x]
+print("Lista multiplicada por 4:", multiplied_list(elements))
