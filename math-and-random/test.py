0c2c58c88754182644bad0422af11ea27362653a
SoyaR08 <rafa140200@gmail.com>
2025-11-13 12:47:30 +0100

    Last exercise in progress

--- math-and-random/test.py
+++ math-and-random/test.py
@@ -1,17 +1,15 @@
 import random
 import math
-# Ejercicio de Random y Math 3
 
-print("========= Ejercicio de Random y Math 3 =========")
+# Ejercicio de Random y Math 7
+print("========= Ejercicio de Random y Math 7 =========")
 
-values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-strips = ["Corazones", "Diamantes", "Tréboles", "Picas"]
-cards = []
-for _ in range(5):
-    random_value_index = random.randint(0, len(values) - 1)
-    random_strip_index = random.randint(0, len(strips) - 1)
-    card = f"{values[random_value_index]} de {strips[random_strip_index]}"
-    cards.append(card)
+possible_numbers = list(range(1, 50))
+number_series = ""
 
-print("Cartas seleccionadas: ", cards)
-print()
+for i in range(6):
+    choice = random.choice(possible_numbers)
+    number_series += " " + str(choice)
+    possible_numbers.remove(choice)
+
+print(number_series)
