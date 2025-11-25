8470c33a878bff57b021c75d4d38a16dd54eac61
SoyaR08 <rafa140200@gmail.com>
2025-11-25 12:28:41 +0100

    Update changes

--- poo_and_files/Employee.py
+++ poo_and_files/Employee.py
@@ -1,3 +1,4 @@
+from typing import final
 from Enterprise import Enterprise
 
 class Employee:
@@ -11,11 +12,43 @@ class Employee:
         self.salary = salary
         self.enterprise = enterprise
 
+    def __str__(self):
+        return f"Empleado Nº: {self._employee_number}, Nombre: {self.name}, Salario: {self.salary}, Empresa: {self.enterprise}"
+
+    def __eq__(self, value):
+        if not isinstance(value, Employee):
+            return False
+        return self.name == value.name and self.salary == value.salary and self.enterprise == value.enterprise
+
+    def set_name(self, name):
+        self.name = name
+
     def get_name(self):
         return self.name
     
+    def set_salary(self, salary):
+        self.salary = salary
+
     def get_salary(self):
         return self.salary
     
+    def set_employee_number(self, employee_number):
+        self._employee_number = employee_number
+
     def get_employee_number(self):
         return self._employee_number
+    
+    @final
+    def raise_salary(self, percentage):
+        self.salary += self.salary * (percentage / 100)
+
+    def dimiss_employee(self):
+        valid = self.enterprise.sack_employee(self.enterprise.locate_employee(self))
+
+        if valid:
+            print(f"{self.name} ha sido despedido de {self.enterprise.get_name()}.")
+            self.enterprise = None
+        else: 
+            print(f"No se ha podido despedir a {self.name} de {self.enterprise.get_name()}.")
+    
+    
--- poo_and_files/Enterprise.py
+++ poo_and_files/Enterprise.py
@@ -1,9 +1,15 @@
+from Employee import Employee
 class Enterprise:
 
     def __init__(self, name, size):
         self.name = name
         self.size = size
         self.employeeList = [None for _ in range(size)]
+
+    def __eq__(self, value):
+        if not isinstance(value, Enterprise):
+            return False
+        return self.name == value.name
 
     def get_name(self):
         return self.name
@@ -21,13 +27,12 @@ class Enterprise:
     
     def hire_employee(self, employee_name, employee_salary):
         if not self._isFull():
-            for i in range(len(self.employeeList)):
-                if self.employeeList[i] is None:
-                    self.employeeList[i] = {
-                        'name': employee_name,
-                        'salary': employee_salary
-                    }
-                    return True
+            new_employee = Employee(employee_name, employee_salary, self)
+            if self.locate_employee(new_employee) == -1:
+                for i in range(len(self.employeeList)):
+                    if self.employeeList[i] is None:
+                        self.employeeList[i] = new_employee
+                        return True
         
         return False
 
@@ -36,3 +41,9 @@ class Enterprise:
             self.employeeList[index] = None
             return True
         return False
+    
+    def locate_employee(self, employee):
+        for i in range(len(self.employeeList)):
+            if self.employeeList[i] is not None and self.employeeList[i]== employee:
+                return i
+        return -1
