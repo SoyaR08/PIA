8f0002d78b5761f107c381837c0c8376345ad284
SoyaR08 <rafa140200@gmail.com>
2025-11-25 17:49:21 +0100

    Starting the csv exercise

---
+++ ficheros_txt_and_csv/exercise1.py
@@ -0,0 +1,12 @@
+import os
+
+def menu():
+    print("==================================")
+    print("1. Crear la tabla de multiplicar de un número")
+    print("2. Leer tabla de multiplicar de un número")
+    print("3. Leer una línea de la tabla de multiplicar de un número")
+    print("4. Salir")
+    print("==================================")
+
+
+menu()
