d8ae06c5d1f862f44968e3d23d609d890bf9e29c
SoyaR08 <rafa140200@gmail.com>
2026-01-13 18:21:41 +0100

    ex3 finished

--- pandas-exercises/ex3.py
+++ pandas-exercises/ex3.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from validations import validate_seven_numeric_items as validate_data
+import matplotlib.pyplot as plt
 
 content = input("Introduce los datos de temperatura de los últimos 7 días en el siguiente formato: dia1,dia2,dia3,dia4,dia5,dia6,dia7 ")
 
@@ -17,4 +18,9 @@ mean = series.mean()
 
 print(mean)
 print(f"Temperatura máxima: {series.idxmax()}, con {series.max()}ºC")
-print(f"Días con temperaturas mayores a la media:\n{series[series > 25.0].to_dict()}")
+print(f"Días con temperaturas mayores a la media:\n{series.gt(25).to_dict()}")
+series = series.apply(lambda x: 25 if pd.isna(x) else x)
+
+plt.title("Temperaturas de la semana")
+plt.plot(series.index, series.values, marker='o')
+plt.show()
