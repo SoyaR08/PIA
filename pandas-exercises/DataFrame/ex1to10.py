5b14399539fc60867630884624a4bd96040b0c4d
SoyaR08 <rafa140200@gmail.com>
2026-01-20 17:47:59 +0100

    Upload my work

--- pandas-exercises/DataFrame/ex1to10.py
+++ pandas-exercises/DataFrame/ex1to10.py
@@ -1,2 +1,56 @@
 import pandas as pd
-from db import df_alumnos as df
+
+df = pd.read_excel('pandas-exercises/DataFrame/datos_alumnos.xlsx')
+
+# 1. Filtra a los alumnos que tienen 20 años o más y muestra sus notas finales.
+
+df_gt_20 = df.loc[df['Edad'] >= 20, [
+    "Nombre", "Apellidos", "Edad", 
+    "Programación T3", "Base de Datos T3", "Lenguajes T3",
+    "Sistemas T3", "Entornos T3"
+]]
+print(df_gt_20)
+
+# 2. Añade una nueva columna llamada 'Aprobado' que indique si el alumno ha aprobado o no (si su promedio general es mayor o igual a 5).
+
+# Copiamos el DataFrame original
+new_df = df.copy()
+
+# Promedios por asignatura (por alumno)
+
+# Creamos la columna Programación
+new_df["Programación"] = df[[ # Esto le dice que va a coger las columnas T1, T2 y T3 de Programación
+    "Programación T1", "Programación T2", "Programación T3"
+]].mean(axis=1) # Mean hace la media y axis le dice si por fila (1) o por columna (0)
+
+new_df["Base de Datos"] = df[[
+    "Base de Datos T1", "Base de Datos T2", "Base de Datos T3"
+]].mean(axis=1)
+
+new_df["Lenguajes"] = df[[
+    "Lenguajes T1", "Lenguajes T2", "Lenguajes T3"
+]].mean(axis=1)
+
+new_df["Sistemas"] = df[[
+    "Sistemas T1", "Sistemas T2", "Sistemas T3"
+]].mean(axis=1)
+
+new_df["Entornos"] = df[[
+    "Entornos T1", "Entornos T2", "Entornos T3"
+]].mean(axis=1)
+
+# Promedio general y aprobado
+new_df["Aprobado"] = new_df[[
+    "Programación", "Base de Datos", "Lenguajes",
+    "Sistemas", "Entornos"
+]].mean(axis=1) >= 5 # Hace que la columna Aprobado sea True o False dependiendo si el promedio es mayor o igual a 5
+
+# Nos quedamos solo con las columnas relevantes
+new_df = new_df[[
+    "Nombre", "Apellidos",
+    "Programación", "Base de Datos", "Lenguajes",
+    "Sistemas", "Entornos",
+    "Aprobado"
+]]
+
+print(new_df)
--- pandas-exercises/DataFrame/test.py
+++ pandas-exercises/DataFrame/test.py
@@ -1,11 +1,58 @@
 import pandas as pd
-from db import df_alumnos as df
 
-# 1. Filtra a los alumnos que tienen 20 años o más y muestra sus notas finales.
+# 3. Calcula la nota máxima de cada módulo y muestra los resultados.
 
-df_gt_20 = df.loc[df['Edad'] >= 20, [
-    "Nombre", "Apellidos", "Edad", 
-    "Programación T3", "Base de Datos T3", "Lenguajes T3",
-    "Sistemas T3", "Entornos T3"
+df = pd.read_excel('pandas-exercises/DataFrame/datos_alumnos.xlsx')
+
+# Copiamos el DataFrame original
+calc_aprv = df.copy()
+
+# Promedios por asignatura (por alumno)
+calc_aprv["Programación"] = df[[
+    "Programación T1", "Programación T2", "Programación T3"
+]].mean(axis=1)
+
+calc_aprv["Base de Datos"] = df[[
+    "Base de Datos T1", "Base de Datos T2", "Base de Datos T3"
+]].mean(axis=1)
+
+calc_aprv["Lenguajes"] = df[[
+    "Lenguajes T1", "Lenguajes T2", "Lenguajes T3"
+]].mean(axis=1)
+
+calc_aprv["Sistemas"] = df[[
+    "Sistemas T1", "Sistemas T2", "Sistemas T3"
+]].mean(axis=1)
+
+calc_aprv["Entornos"] = df[[
+    "Entornos T1", "Entornos T2", "Entornos T3"
+]].mean(axis=1)
+
+# Promedio general y aprobado
+calc_aprv["Aprobado"] = calc_aprv[[
+    "Programación", "Base de Datos", "Lenguajes",
+    "Sistemas", "Entornos"
+]].mean(axis=1) >= 5
+
+# Nos quedamos solo con las columnas relevantes
+calc_aprv = calc_aprv[[
+    "Nombre", "Apellidos",
+    "Programación", "Base de Datos", "Lenguajes",
+    "Sistemas", "Entornos",
+    "Aprobado"
 ]]
-print(df_gt_20.to_dict())
+
+asignaturas = ["Programación", "Base de Datos", "Lenguajes", "Sistemas", "Entornos"]
+notas = calc_aprv[asignaturas]
+
+idx_max = notas.idxmax(axis=0)
+data = {
+    "Nombre": calc_aprv.loc[idx_max, "Nombre"].values,
+    "Apellidos": calc_aprv.loc[idx_max, "Apellidos"].values,
+    "Nota": notas.max(axis=0).values
+}
+
+result_df = pd.DataFrame(data, index=asignaturas)
+result_df.index.name = None  # opcional, para que el índice sea limpio
+
+print(result_df)
